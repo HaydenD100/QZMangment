dfaaec2e1ff9bdc79651f4da72c51c89c5dc1d46
kokok <kokok@DESKTOP-S38C519>
2025-11-22 19:17:10 -0500

    Initial commit

---
+++ src/common.py
@@ -0,0 +1,16 @@
+class Software:
+    def __init__(self):
+        self.ID
+        self.Name
+        self.CVSS
+        self.Summary
+        self.Recommnedation
+        self.LastScan #Time Stamp
+        self.UserID
+        pass
+class User:
+    def __init__(self):
+        self.ID
+        self.HashedPassword
+        self.Name
+        pass
